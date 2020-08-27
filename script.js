2e83f2739d1467464aeb67882bc03cba3b7d715f
oreillyalan <oreillyalan132@gmail.com>
2020-08-27 17:31:37 +0100

    added validation to modal forms

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
-// Event Handler
+// Event Handlers
 function showModal(){
     modal.classList.add('show-modal');
     websiteNameEl.focus();
@@ -16,10 +16,31 @@ function removeModal(){
 }
 
 function isModalShowingCheck(e){
-   e.target === modal ? removeModal() : false; 
+    e.target === modal ? removeModal() : false; 
 }
 
-// Event Listener
+function storeBookmarkFunction(e){
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    !urlValue.includes('http://','https://') ? urlValue = `https://${urlValue}` : false;
+    console.log(urlValue);
+    validate(nameValue, urlValue);
+}
+
+
+// Event Listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e)=> isModalShowingCheck(e)); 
+bookmarkForm.addEventListener('submit', storeBookmarkFunction);
+
+
+// Validate Form
+function validate(nameValue, urlValue){
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    (!nameValue || !urlValue) ? (alert('Please enter data for both fields') , false): true;
+    urlValue.match(regex) ? true: alert('Please provide a valid web address');
+
+}
